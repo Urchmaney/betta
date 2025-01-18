86c11de1a9b1b8c477bf8e0ccd28e38947ffcf65
Unegbu Kingsley <kingsobino@gmail.com>
2025-01-18 22:10:19 +0100

    use jwt for token generation

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -10,11 +10,18 @@ class ApplicationController < ActionController::API
 
   private
     def authenticate
-      if session_record = authenticate_with_http_token { |token, _| Session.find_signed(token) }
+      if session_record = authenticate_with_http_token { |token, _| session_from_token(token) }
         Current.session = session_record
       else
         request_http_token_authentication
       end
+    end
+
+    def session_from_token(token)
+      data = JWT.decode(token, ENV.fetch("APP_SECRET", Rails.application.secret_key_base), true)[0]
+      Session.find(data["session_id"])
+    rescue
+      render json: { error: "Invalid Detail" }, status: :unauthorized
     end
 
     def set_current_request_details
