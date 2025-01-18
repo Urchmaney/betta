7e51f0db3fcbbd05cffbe29b797550950d60b761
Unegbu Kingsley <kingsobino@gmail.com>
2025-01-18 22:16:58 +0100

    map jobs

--- websocker-server/src/socket.ts
+++ websocker-server/src/socket.ts
@@ -2,19 +2,28 @@
 import { AxiosResponse } from 'axios';
 import WebSocket, { WebSocketServer } from 'ws';
 import { fetchLeaderboard } from './api';
+import { addNewBetsJob, addNewGamesJob, addNewUsersJob } from './redis';
 
-export function createSocketServer (port: number) : WebSocket.WebSocketServer{
-  const wss: WebSocket.Server = new WebSocketServer({ port });
+const jobMap: Record<string, (args: (string | number)[], queue?: string) => void> = {
+  "newGames": addNewGamesJob,
+  "newUsers": addNewUsersJob,
+  "newBets": addNewBetsJob
+}
 
+export function createSocketServer () : WebSocket.WebSocketServer{
+  const wss: WebSocket.Server = new WebSocketServer({ noServer: true });
+
+  
   wss.on("connection", (ws: WebSocket) => {
     fetchLeaderboard().then((response: AxiosResponse) => {
       ws.send(response.data as string)
     });
 
     ws.on("message", (message: WebSocket.RawData) => {
-      
+      const messageObj = JSON.parse(message.toString());
+      jobMap[messageObj.type]([messageObj.data])
     })
-  })
+  });
   
   return wss;
 }
