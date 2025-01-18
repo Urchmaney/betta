eec69b3dbdff62e1156b52e7ae2f4a86befb8d4c
Unegbu Kingsley <kingsobino@gmail.com>
2025-01-18 22:15:58 +0100

    add authentication for websocket

--- websocker-server/server.ts
+++ websocker-server/server.ts
@@ -1,15 +1,34 @@
 import 'dotenv/config'
-import { Redis } from "ioredis";
 import { LeaderboardSubscription } from './src/leaderboard.subscription';
-import { redisConnection } from './src/redis';
+
 import { createSocketServer } from './src/socket';
+import { createServer, IncomingMessage } from 'http';
+import { Duplex } from 'stream';
+import { redis } from "./src/redis";
+import { authenticateToken } from './src/auth';
+
+const wss = createSocketServer()
+const server = createServer();
+
+server.on("upgrade", function (request: IncomingMessage, socket: Duplex, head: Buffer) {
+  authenticateToken(request,
+    () => { 
+      wss.handleUpgrade(request, socket, head, function done(ws) {
+        wss.emit('connection', ws, request);
+      });
+    },
+    (err) => {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+      socket.destroy();
+      return;
+    })
+})
 
 const port: number = Number(process.env.PORT || 8080)
 
-const wss = createSocketServer(port)
-
-const redis: Redis = new Redis(redisConnection())
 
 new LeaderboardSubscription(redis, wss);
+
+server.listen(port)
 
 console.log(`Listening at port ${port}`)
