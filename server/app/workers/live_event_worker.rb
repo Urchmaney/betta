7d7febf85772d7857ec979891e6037bde792b50c
Unegbu Kingsley <kingsobino@gmail.com>
2025-01-19 15:48:50 +0100

    live event worker and publishing leaderboard changes

---
+++ server/app/workers/live_event_worker.rb
@@ -0,0 +1,37 @@
+class LiveEventWorker
+  include Sidekiq::Worker
+
+  def perform(*args)
+    games = JSON.parse(args[0])
+    games.map do |game|
+      created = false
+      found_game = Game.find_or_create_by(external_id: game['gameId']) do |g|
+        created = true
+        g.home_team = game['homeTeam']
+        g.away_team = game['awayTeam']
+        g.home_score = game['homeScore']
+        g.away_score = game['awayScore']
+        g.time_elapsed = game['timeElapsed']
+      end
+
+      if !created
+        found_game.home_score = game['homeScore'] if game['homeScore'].present?
+        found_game.away_score = game['awayScore'] if game['awayScore'].present?
+        found_game.time_elapsed = game['timeElapsed'] if game['timeElapsed'].present?
+
+        found_game.save
+      end
+
+      found_game.events.create(
+        game['events'].map do |event|
+          {
+            event_type: event['type'],
+            for_home: event['team'] == 'home',
+            player: event['player'],
+            minute: event['minute']
+          }
+        end
+      )
+    end
+  end
+end
