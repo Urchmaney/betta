79cadda8c5ccb0b6629c60efd49a1dca07fafd41
Unegbu Kingsley <kingsobino@gmail.com>
2025-01-19 08:40:01 +0100

    fetch redis configuration from env variable

--- server/config/initializers/redis.rb
+++ server/config/initializers/redis.rb
@@ -1,6 +1,7 @@
 # Server::Redis = Redis.new(host: redis_host, port: redis_port.to_i)
 
-REDIS_CONFIG = YAML.load( File.open(Rails.root.join("config/redis.yml"))).symbolize_keys
+
+REDIS_CONFIG = YAML.load(ERB.new(File.read(Rails.root.join("config/redis.yml"))).result).symbolize_keys
 config = REDIS_CONFIG[:default].symbolize_keys
 config = config.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
 
